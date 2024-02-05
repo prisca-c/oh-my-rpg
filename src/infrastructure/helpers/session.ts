7665956ab81ed0f91b93364d97b577939f02e2e6
Prisca <prisca.castanier@gmail.com>
2024-02-06 00:18:05 +0100

    feat(session): add if statement to session to avoid throw exception

--- src/infrastructure/helpers/session.ts
+++ src/infrastructure/helpers/session.ts
@@ -1,5 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export const getErrorByField = (ctx: HttpContext, field: string) => {
+  if (!ctx.session || !ctx.session.flashMessages) {
+    return
+  }
   return ctx.session.flashMessages.get('errors')?.[field]?.[0]
 }
