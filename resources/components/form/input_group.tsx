3c7bb21b58be2eec298540fcc0cd84a71d48688f
Prisca <prisca.castanier@gmail.com>
2024-02-09 00:35:52 +0100

    feat(input): update input group to handle validation, errors , onChange

--- resources/components/form/input_group.tsx
+++ resources/components/form/input_group.tsx
@@ -1,10 +1,18 @@
+import React from 'react'
+
+import { getErrorByField } from '@/helpers/form_helpers'
+
 type InputGroupProps = {
   label: string
   name: string
   type: string
   id: string
-  error?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  errors?: any
   autoComplete?: string
+  className?: string
+  valid?: boolean
   props?: any
 }
 
@@ -13,20 +21,34 @@ export const InputGroup = ({
   name,
   type,
   id,
-  error,
+  value,
+  errors,
   autoComplete,
+  onChange,
+  valid,
+  className,
   ...props
 }: InputGroupProps) => {
+  const error = getErrorByField(errors, name)
+
+  const inputClass = valid
+    ? `border-2 border-green-500 ${className}`
+    : error
+      ? `border-2 border-red-500 ${className}`
+      : `border-2 border-gray-300 ${className}`
+
   return (
     <div className={'flex flex-col'}>
-      {error && <p className={'text-red-500'}>{error}</p>}
+      {error && <p className={'text-red-500 text-xs'}>{error}</p>}
       <label htmlFor={name}>{label}</label>
       <input
         type={type}
         name={name}
         id={id}
-        className={'border-2 border-gray-300 rounded-md p-2'}
+        className={inputClass + ' p-2 rounded-md'}
         autoComplete={autoComplete}
+        value={value}
+        onChange={onChange}
         {...props}
       />
     </div>
