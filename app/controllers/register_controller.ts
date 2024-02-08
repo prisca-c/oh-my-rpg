0717ffe6abc48121a1009a39367e2682e400dd9e
Prisca <prisca.castanier@gmail.com>
2024-02-09 00:37:31 +0100

    feat(register) : add csrf token to props and hanle meta infos

--- app/controllers/register_controller.ts
+++ app/controllers/register_controller.ts
@@ -1,7 +1,18 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class RegisterController {
-  async index({ inertia }: HttpContext) {
-    return inertia.render('public/register')
+  async index(ctx: HttpContext) {
+    return ctx.inertia.render(
+      'public/register',
+      {
+        csrfToken: ctx.request.csrfToken,
+      },
+      {
+        meta: {
+          title: 'Register',
+          description: 'Register for an account',
+        },
+      }
+    )
   }
 }
