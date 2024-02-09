ec61b0e7e396b7c80d772d45caa02785e74e4c9a
Prisca C <prisca.castanier@gmail.com>
2024-02-09 11:47:51 +0100

    feat(login): setup login page and add csrf token and meta to controller

--- app/controllers/login_controller.ts
+++ app/controllers/login_controller.ts
@@ -1,7 +1,16 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LoginController {
-  async index({ inertia }: HttpContext) {
-    return inertia.render('public/login')
+  async index({ inertia, request }: HttpContext) {
+    return inertia.render(
+      'public/login',
+      { csrfToken: request.csrfToken },
+      {
+        meta: {
+          title: 'Login',
+          description: 'Login to your account',
+        },
+      }
+    )
   }
 }
--- resources/pages/public/login.tsx
+++ resources/pages/public/login.tsx
@@ -2,18 +2,55 @@ import React from 'react'
 import { router } from '@inertiajs/react'
 
 import { Button } from '@/components/button'
+import { Form } from '@/components/form/form'
+import { useLoginForm } from '@/hooks/use_login_form'
 import { Typography } from '@/components/utils/typography'
+import { InputGroup } from '@/components/form/input_group'
 
-export default function Login() {
+type LoginProps = {
+  csrfToken: string
+}
+
+export default function Login({ csrfToken }: LoginProps) {
+  const { inputs, validations, onChange, onSubmit } = useLoginForm({
+    email: '',
+    password: '',
+  })
+
   const goToRegister = () => {
     router.visit('/register')
   }
+
   return (
     <div>
       <Typography type={'h1'} size={'5xl'} className={'font-bold text-center'}>
         Login
       </Typography>
-      <Button onClick={goToRegister}>Register</Button>
+      <Form method={'POST'} onSubmit={onSubmit}>
+        <input type="hidden" name="_csrf" value={csrfToken} />
+        <InputGroup
+          label={'Email'}
+          name={'email'}
+          type={'email'}
+          id={'email'}
+          autoComplete={'email'}
+          valid={validations.email}
+          value={inputs.email}
+          onChange={onChange}
+        />
+        <InputGroup
+          label={'Password'}
+          name={'password'}
+          type={'password'}
+          id={'password'}
+          value={inputs.password}
+          onChange={onChange}
+        />
+        <Button type={'submit'}>Login</Button>
+        <Button type={'button'} onClick={goToRegister}>
+          Register
+        </Button>
+      </Form>
     </div>
   )
 }
