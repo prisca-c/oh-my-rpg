a6ed0e77a9154fd9a60a68856cdf25ca8df782f6
Prisca <prisca.castanier@gmail.com>
2024-02-10 15:33:29 +0100

    feat(character): add a create character form

---
+++ resources/hooks/use_create_character_form.ts
@@ -0,0 +1,35 @@
+import type React from 'react'
+import { router } from '@inertiajs/react'
+import { useEffect, useState } from 'react'
+
+type CreateCharacterForm = {
+  name: string
+}
+
+type FormValidations = {
+  name: boolean
+}
+
+export const useCreateCharacterForm = (initialValues: CreateCharacterForm) => {
+  const [inputs, setInputs] = useState<CreateCharacterForm>(initialValues)
+  const [validations, setValidations] = useState<FormValidations>({
+    name: false,
+  })
+  useEffect(() => {
+    setValidations({
+      name: inputs.name.length > 3,
+    })
+  }, [inputs])
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setInputs((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    router.post('/characters', new FormData(e.currentTarget))
+  }
+
+  return { inputs, validations, onChange, onSubmit }
+}
--- resources/pages/private/characters.tsx
+++ resources/pages/private/characters.tsx
@@ -1,19 +1,47 @@
+import { usePage } from '@inertiajs/react'
 import type Character from '~/app/models/character'
 
 import { Button } from '@/components/button'
 import { CharacterCard } from '@/components/character_card'
 import { Flex, Typography, Form, InputGroup } from '@/components/utils'
+import { useCreateCharacterForm } from '@/hooks/use_create_character_form'
 
 type CharactersProps = {
   characters: Character[]
 }
 
 export default function Characters({ characters }: CharactersProps) {
+  const { inputs, validations, onChange, onSubmit } = useCreateCharacterForm({
+    name: '',
+  })
+
+  const errors = usePage().props.errors
+
   return (
-    <Flex direction={'row'} justify={'center'} align={'center'} className={'gap-6'}>
-      {characters.map((character) => (
-        <CharacterCard key={character.id} character={character} />
-      ))}
+    <Flex direction={'col'} justify={'center'} align={'center'} className={'gap-6'}>
+      <Typography type={'h1'} size={'5xl'} className={'font-bold text-center'}>
+        Characters
+      </Typography>
+      <Form method={'POST'} onSubmit={onSubmit}>
+        <InputGroup
+          label={'Name'}
+          name={'name'}
+          type={'text'}
+          id={'name'}
+          errors={errors}
+          value={inputs.name}
+          valid={validations.name}
+          onChange={onChange}
+          autoComplete={'name'}
+          className={inputs.name ? 'border-2 border-green-500' : ''}
+        />
+        <Button type={'submit'}>Create</Button>
+      </Form>
+      <Flex direction={'row'} justify={'center'} align={'center'} className={'gap-6'}>
+        {characters.map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+      </Flex>
     </Flex>
   )
 }
