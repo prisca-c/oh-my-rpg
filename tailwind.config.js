0e879a59bc9cf8cf46d718938c65b73d750dac08
Prisca <prisca.castanier@gmail.com>
2024-02-10 23:08:48 +0100

    add new fonts to the projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./resources/**/*.{edge,js,ts,jsx,tsx,vue}'],
   prefix: '',
   theme: {
+    fontFamily: {
+      creato: ['CreatoDisplay', 'sans-serif'],
+      gamer: ['Gamer', 'sans-serif'],
+    },
     container: {},
     extend: {},
   },
