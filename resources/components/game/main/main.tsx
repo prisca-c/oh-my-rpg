a0780c898ddd17eccc3a82a79e1105ecfdad805a
Prisca <prisca.castanier@gmail.com>
2024-02-11 20:25:26 +0100

    feat(game); add main component to handle main scene

---
+++ resources/components/game/main/main.tsx
@@ -0,0 +1,19 @@
+import { Container, Typography } from '@/components/utils'
+
+export const Main = () => {
+  return (
+    <Container
+      layout={'flex'}
+      direction={'col'}
+      justify={'center'}
+      align={'center'}
+      className={'gap-6 h-full w-[500px]'}
+      rounded
+      bg={'lightPrimary'}
+    >
+      <Typography type={'h1'} size={'xl'} className={'font-bold text-center'}>
+        Main
+      </Typography>
+    </Container>
+  )
+}
--- resources/pages/private/game.tsx
+++ resources/pages/private/game.tsx
@@ -1,6 +1,7 @@
 import type Character from '~/app/models/character'
 import type EntityProperty from '~/app/models/entity_property'
 
+import { Main } from '@/components/game/main/main'
 import { Flex, Typography } from '@/components/utils'
 import { Leaderboard } from '@/components/game/leaderboard'
 import { CharacterInfos } from '@/components/game/character_infos'
@@ -17,8 +18,9 @@ export default function Game({ character, leaderboard, properties }: GameProps) 
       <Typography type={'h1'} size={'xl'} className={'font-bold text-center'}>
         Game
       </Typography>
-      <Flex direction={'row'} justify={'center'} align={'flex-start'} className={'gap-6'}>
+      <Flex direction={'row'} justify={'center'} align={'center'} className={'gap-6 h-[400px]'}>
         <CharacterInfos character={character} properties={properties} />
+        <Main />
         <Leaderboard leaderboard={leaderboard} />
       </Flex>
     </Flex>
