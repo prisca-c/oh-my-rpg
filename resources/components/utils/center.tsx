918a925cd42b13b01e20d54257149fa2db88b18f
Prisca <prisca.castanier@gmail.com>
2024-02-24 21:03:03 +0100

    feat(center): add className props to center component

--- resources/components/utils/center.tsx
+++ resources/components/utils/center.tsx
@@ -4,13 +4,19 @@ type CenterProps = {
   children: React.ReactNode
   type?: 'grid' | 'flex'
   heightFull?: boolean
+  className?: string
 }
 
-export const Center = ({ children, type = 'grid', heightFull = false }: CenterProps) => {
+export const Center = ({
+  children,
+  type = 'grid',
+  heightFull = false,
+  className = '',
+}: CenterProps) => {
   const centerType = {
     grid: 'grid place-items-center',
     flex: 'flex justify-center items-center',
   }
-  const classes = `${centerType[type]} ${heightFull ? 'h-full' : ''}`
+  const classes = [centerType[type], heightFull ? 'h-full' : '', className].join(' ')
   return <div className={classes}>{children}</div>
 }
