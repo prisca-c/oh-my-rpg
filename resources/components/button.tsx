c21f10e975f56feaded2e23e4cbe748654153235
Prisca <prisca.castanier@gmail.com>
2024-02-24 19:56:00 +0100

    feat(button): add bgColor props to button component

--- resources/components/button.tsx
+++ resources/components/button.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 
+import { BgColors } from '@/enums/tailwind'
+
 type ButtonProps = {
   children: React.ReactNode
   type?: 'button' | 'reset' | 'submit'
   onClick?: () => void
+  color?: keyof typeof BgColors
 }
 
 export const Button = ({
   children,
   type = 'button',
   onClick = () => {},
+  color = 'primary',
 }: ButtonProps): React.ReactElement => {
+  const bgColors = BgColors[color]
+  const classList = ['px-4', 'py-2', bgColors, 'text-white', 'rounded-md']
+  const classString = classList.join(' ')
+
   return (
-    <button type={type} onClick={onClick} className="px-4 py-2 bg-blue-500 text-white rounded-md">
+    <button type={type} onClick={onClick} className={classString}>
       {children}
     </button>
   )
