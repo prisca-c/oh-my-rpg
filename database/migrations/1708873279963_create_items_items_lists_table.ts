0227cfb9e05bd04eafcbfebaaa5ba16af5551b33
Prisca <prisca.castanier@gmail.com>
2024-02-25 16:37:38 +0100

    feat(items): add migration for item_list and item_item_list table

---
+++ database/migrations/1708873279963_create_items_items_lists_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'items_item_lists'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').defaultTo(this.raw('gen_random_uuid()')).primary()
+      table.uuid('item_id').references('id').inTable('items').onDelete('CASCADE')
+      table.uuid('item_list_id').references('id').inTable('item_lists').onDelete('CASCADE')
+      table.float('drop_chance').notNullable()
+      table.boolean('only_boss').defaultTo(false)
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
