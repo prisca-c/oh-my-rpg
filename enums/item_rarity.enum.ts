dcfd0dcdd63f95709eccc7f8149847cfb922141a
Prisca <prisca.castanier@gmail.com>
2024-02-26 01:22:13 +0100

    feat(item): add item_rarity enum

---
+++ enums/item_rarity.enum.ts
@@ -0,0 +1,10 @@
+export enum ItemRarity {
+  RARE = 'rare',
+  EPIC = 'epic',
+  QUEST = 'quest',
+  COMMON = 'common',
+  MYTHIC = 'mythic',
+  SECRET = 'secret',
+  UNCOMMON = 'uncommon',
+  LEGENDARY = 'legendary',
+}
