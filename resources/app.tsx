1396e4f05f2d2308e778c9654ace21723178710e
Prisca <prisca.castanier@gmail.com>
2024-03-03 14:09:23 +0100

    refactor: move app.css loading to app_root

--- resources/app.tsx
+++ resources/app.tsx
@@ -1,5 +1,3 @@
-import './css/app.css'
-
 import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react'
 import type { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react'
