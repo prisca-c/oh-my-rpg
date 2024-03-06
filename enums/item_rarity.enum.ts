414afe465283241c4ce8c35d977a53cfc62fec9f
Prisca <prisca.castanier@gmail.com>
2024-03-06 23:10:58 +0100

    feat(item): add rarity drop chance

--- enums/item_rarity.enum.ts
+++ enums/item_rarity.enum.ts
@@ -8,3 +8,14 @@ export enum ItemRarity {
   UNCOMMON = 'uncommon',
   LEGENDARY = 'legendary',
 }
+
+export const itemRarityDropChance: Record<ItemRarity, number> = {
+  [ItemRarity.RARE]: 0.4,
+  [ItemRarity.EPIC]: 0.2,
+  [ItemRarity.QUEST]: 0.5,
+  [ItemRarity.COMMON]: 0.8,
+  [ItemRarity.MYTHIC]: 0.01,
+  [ItemRarity.SECRET]: 0.001,
+  [ItemRarity.UNCOMMON]: 0.7,
+  [ItemRarity.LEGENDARY]: 0.05,
+} as const
