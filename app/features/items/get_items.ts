65e19cc5292624e4767f683a055899464bd03b8d
Prisca <prisca.castanier@gmail.com>
2024-03-10 18:25:13 +0100

    fix(getItems): now return a typed list to avoid issue with Item[] type

--- app/features/items/get_items.ts
+++ app/features/items/get_items.ts
@@ -5,17 +5,17 @@ import type ItemList from '#models/item_list'
 export default class GetItems {
   async handle(world: World): Promise<Item[]> {
     await world.load('itemLists', (itemListsQuery) => {
-      itemListsQuery.preload('items', (itemsQuery) => {
-        itemsQuery.preload('itemRarity')
+      itemListsQuery.preload('items')
+    })
+
+    const lists: Item[] = []
+
+    world.itemLists.map((itemList: ItemList) => {
+      itemList.items.map((item) => {
+        lists.push(item)
       })
     })
 
-    const items = world.itemLists.map((itemList: ItemList) => {
-      return itemList.items
-    })
-
-    console.log('GetItems.handle', items)
-
-    return items as unknown as Item[]
+    return lists
   }
 }
