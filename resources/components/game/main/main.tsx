a3e3655a31fde2bf6787d1362d2eaf370ce5e121
Prisca <prisca.castanier@gmail.com>
2024-03-10 18:22:45 +0100

    feat(loot): world card is now clickable and return a loot

--- resources/components/game/main/main.tsx
+++ resources/components/game/main/main.tsx
@@ -3,6 +3,11 @@ import { Container, Typography, Card } from '@/components/utils'
 
 export const Main = () => {
   const worlds = useWorldsStore((state) => state.worlds)
+  const getItem = async (worldId: string) => {
+    const res = await fetch(`/world/loot/${worldId}`)
+    return await res.json()
+  }
+
   return (
     <Container
       layout={'flex'}
@@ -17,11 +22,13 @@ export const Main = () => {
         Main
       </Typography>
       {worlds.map((world) => (
-        <Card key={world.id}>
-          <Typography type={'p'} size={'sm'} className={'font-bold'}>
-            {world.name}
-          </Typography>
-        </Card>
+        <a key={world.id} className={'cursor-pointer'} onClick={() => getItem(world.id)}>
+          <Card key={world.id}>
+            <Typography type={'p'} size={'sm'} className={'font-bold'}>
+              {world.name}
+            </Typography>
+          </Card>
+        </a>
       ))}
     </Container>
   )
--- start/routes.ts
+++ start/routes.ts
@@ -12,6 +12,7 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
 
 // region -- Import controllers
+const LootController = () => import('#controllers/loot_controller')
 const RegisterController = () => import('#controllers/register_controller')
 const LoginController = () => import('#controllers/login_controller')
 const HomeController = () => import('#controllers/home_controller')
@@ -29,11 +30,11 @@ router.get('/logout', [AuthController, 'logout'])
 
 router.get('/register', [RegisterController, 'index']).as('register.get')
 router.post('/register', [AuthController, 'register']).as('register.post')
-
 router
   .group(() => {
     router.get('/game/:id', [GamesController, 'index']).where('id', router.matchers.uuid())
     router.get('/characters', [CharactersController, 'index'])
     router.post('/characters', [CharactersController, 'store']).as('character.store')
+    router.get('/world/loot/:id', [LootController, 'handle']).where('id', router.matchers.uuid())
   })
   .use(middleware.auth())
