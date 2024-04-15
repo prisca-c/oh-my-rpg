9f78f07404fb134af37b9b077d50cdc24d617b59
Prisca <prisca.castanier@gmail.com>
2024-04-15 23:18:02 +0200

    chore: add soft delete package for lucid

--- adonisrc.ts
+++ adonisrc.ts
@@ -64,6 +64,7 @@ export default defineConfig({
     () => import('@adonisjs/inertia/inertia_provider'),
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/static/static_provider'),
+    () => import('adonis-lucid-soft-deletes/provider'),
   ],
 
   /*
